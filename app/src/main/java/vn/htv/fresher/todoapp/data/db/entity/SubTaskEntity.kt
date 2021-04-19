6f3a25619d1dcaaf780c1228a09b524efad18544
Do Quang Sang <sangdq@hybrid-technologies.vn>
2021-04-19 14:44:52 +0700

    category-subtask-model-entity

--- app/src/main/java/vn/htv/fresher/todoapp/data/db/entity/SubTaskEntity.kt
+++ app/src/main/java/vn/htv/fresher/todoapp/data/db/entity/SubTaskEntity.kt
@@ -8,27 +8,27 @@ import vn.htv.fresher.todoapp.domain.model.SubTaskModel
 
 @Entity (tableName = SubTask.NAME)
 data class SubTask(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = COLUMN_ID)         val id: Int,
-    @ColumnInfo(name = COLUMN_TASKID)     val taskid: Int,
-    @ColumnInfo(name = COLUMN_NAME)       val name: String,
-    @ColumnInfo(name = COLUMN_FINISHED)   val finished: Boolean? = null,
-    @ColumnInfo(name = COLUMN_CREATEDAT)  val createdat: LocalDateTime
-){
-    companion object {
-        const val NAME             = "subtask"
-        const val COLUMN_ID        = "id"
-        const val COLUMN_TASKID    = "taskid"
-        const val COLUMN_NAME      = "name"
-        const val COLUMN_FINISHED  = "finished"
-        const val COLUMN_CREATEDAT = "createdat"
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = COLUMN_ID)          val id         : Int,
+  @ColumnInfo(name = COLUMN_TASK_ID)     val taskId     : Int,
+  @ColumnInfo(name = COLUMN_NAME)        val name       : String,
+  @ColumnInfo(name = COLUMN_FINISHED)    val finished   : Boolean? = null,
+  @ColumnInfo(name = COLUMN_CREATED_AT)  val createdAt  : LocalDateTime
+) {
+  companion object {
+    const val NAME               = "sub_task"
+    const val COLUMN_ID          = "id"
+    const val COLUMN_TASK_ID     = "task_id"
+    const val COLUMN_NAME        = "name"
+    const val COLUMN_FINISHED    = "finished"
+    const val COLUMN_CREATED_AT  = "created_at"
 
-        fun fromModel(model: SubTaskModel) = SubTask(
-            id        = model.id,
-            taskid    = model.taskid,
-            name      = model.name,
-            finished  = model.finished,
-            createdat = model.createdat
-        )
-    }
+    fun fromModel(model: SubTaskModel) = SubTask(
+      id         = model.id,
+      taskId     = model.taskId,
+      name       = model.name,
+      finished   = model.finished,
+      createdAt  = model.createdAt
+    )
+  }
 }
