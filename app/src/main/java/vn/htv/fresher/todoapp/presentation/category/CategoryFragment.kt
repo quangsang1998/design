7abec6477368d7efe0f3f9076f016265782b4414
Do Quang Sang <sangdq@hybrid-technologies.vn>
2021-05-05 10:03:23 +0700

    screen-task-list

--- app/src/main/java/vn/htv/fresher/todoapp/presentation/category/CategoryFragment.kt
+++ app/src/main/java/vn/htv/fresher/todoapp/presentation/category/CategoryFragment.kt
@@ -1,7 +1,6 @@
 package vn.htv.fresher.todoapp.presentation.category
 
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.input.input
 import kotlinx.android.synthetic.main.fragment_category.*
@@ -9,76 +8,104 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.threeten.bp.LocalDateTime
 import vn.htv.fresher.todoapp.R
 import vn.htv.fresher.todoapp.databinding.FragmentCategoryBinding
-import vn.htv.fresher.todoapp.domain.model.CategoryModel
 import vn.htv.fresher.todoapp.domain.model.TaskModel
 import vn.htv.fresher.todoapp.presentation.common.BaseFragment
-import vn.htv.fresher.todoapp.presentation.common.decoration.DefaultItemDecoration
 
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>() {
 
-  //val mesage: String? = getArguments()?.getString("category")
-  override val layoutId: Int
-    get() = R.layout.fragment_category
+    // val mesage: String? = getArguments()?.getString("category")
+    override val layoutId: Int
+        get() = R.layout.fragment_category
 
-  private val viewModel by viewModel<CategoryViewModel>()
+    private val viewModel by viewModel<CategoryViewModel>()
 
-  private val taskAdapter by lazy {
-    TaskAdapter(importantCallback = {
-      // Xu ly cap nhat database truong important cho task model
-      viewModel.updateImportant(it)
-    },
-    finishedCallback = {
-      viewModel.updateComplete(it)
-    })
-  }
+    private val taskAdapter by lazy {
+        TaskAdapter(
+            importantCallback = {
+                // Xu ly cap nhat database truong important cho task model
+                viewModel.updateImportant(it)
+            },
+            finishedCallback = {
+                viewModel.updateComplete(it)
+            }
+        )
+    }
 
-  override fun init() {
-    super.init()
-    viewModel.insertTask()
+    override fun init() {
+        super.init()
+        //       viewModel.insertTask()
 //    viewModel.insertTask()
 //    viewModel.insertTask()
 //    viewModel.insertCategory()
-    viewModel.loadData()
+        viewModel.loadData()
+        binding.event = EventAddTask()
+    }
 
-  }
-  override fun initUi() {
-    super.initUi()
+    override fun initUi() {
+        super.initUi()
 //    cat.apply {
 //      text = mesage
 //    }
-    recycleView1.apply {
-      adapter = taskAdapter
-//      addItemDecoration(
-//        DefaultItemDecoration(
-//        resources.getDimensionPixelSize(R.dimen.recyclerview_item_horizontal_margin),
-//        resources.getDimensionPixelSize(R.dimen.recyclerview_item_vertical_margin))
-//      )
+        recycleView1.apply {
+            adapter = taskAdapter
+        }
     }
-  }
 
-  override fun registerLiveDataListener() {
-    super.registerLiveDataListener()
+    override fun registerLiveDataListener() {
+        super.registerLiveDataListener()
 
-    viewModel.itemList.observe(this, {
-      taskAdapter.setItems(it)
-    })
+        viewModel.itemList.observe(
+            this,
+            {
+                taskAdapter.setItems(it)
+            }
+        )
 
-    viewModel.updateImportantCompleted.observe(this, {
-      viewModel.loadData()
-    })
+        viewModel.updateImportantCompleted.observe(
+            this,
+            {
+                viewModel.loadData()
+            }
+        )
 
 //    viewModel.updateFinishedCompleted.observe(this, {
 //      viewModel.loadData()
 //    })
-  }
-  /**
-   * Static definition
-   */
-  companion object {
+        viewModel.addTaskCompleted.observe(
+            this@CategoryFragment,
+            Observer {
+              if (!it) return@Observer
+              viewModel.loadData()
+            }
+        )
+    }
+
+    inner class EventAddTask() {
+        fun onNewTask() {
+            MaterialDialog(safeContext).show {
+                input(
+                    hint = resources.getString(R.string.new_task_hint),
+                ) { _, title ->
+                    val model = TaskModel(
+                        name = title.toString(),
+                        createdAt = LocalDateTime.now()
+                    )
+                    viewModel.addNewTask(model)
+                }
+                positiveButton(R.string.button_create_task)
+                negativeButton(R.string.button_delete)
+            }
+        }
+    }
 
     /**
-     * Create MainFragment instance pattern
+     * Static definition
      */
-    fun newInstance() = CategoryFragment()
-  }
+    companion object {
+
+        /**
+         * Create MainFragment instance pattern
+         */
+        fun newInstance() = CategoryFragment()
+    }
 }
